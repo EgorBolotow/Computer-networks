51ee2a4c714738cfb9fa3dd222871cf7bfa43350
egorkasprigorca <71177475+EgorBolotow@users.noreply.github.com>
2022-04-10 22:10:52 +0300

    Create signals.py

---
+++ signals.py
@@ -0,0 +1,32 @@
+import sounddevice as sd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import random
+
+duration = 0.3  # длительность сигнала в секундах
+amplitude = 0.5  # амплитуда  (в пределах: +-1.0)
+frequency = 293.6648  # частота сигнала в [Гц]
+# т.к. невозможно программно организовать аналоговый сигнал, необходимо обозначить
+fs = 80000 # 80 тыс. временных отсчетов в 1 секунду количество временных отчетов, т.е. частоту дискретизации (об этом чуть позже)
+
+
+timeSamples = np.arange(np.ceil(duration * fs)) / fs
+timeSamples
+
+signal2 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
+signal3 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
+signal4 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
+signal5 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
+signalSumm = signal2 + signal3 + signal4 + signal5
+
+for i  in range(0, 2):
+    sd.play(signal2, fs, None, 1)
+    sd.play(signal3, fs, None, 1)
+    sd.play(signal4, fs, None, 1)
+    sd.play(signal2, fs, None, 1)
+    sd.play(signal3, fs, None, 1)
+
+plt.plot(timeSamples[:2000] * 1000, signalSumm[:2000])
+plt.title("sine tone")
+plt.xlabel("time / milliseconds")
