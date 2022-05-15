bd5411963e364d184f82ca370c06ee55f2b491c7
egorkasprigorca <71177475+EgorBolotow@users.noreply.github.com>
2022-05-15 15:36:48 +0300

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,8 +1,5 @@
 import sounddevice as sd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import random
 
 duration = 0.3  # длительность сигнала в секундах
 amplitude = 0.5  # амплитуда  (в пределах: +-1.0)
@@ -10,23 +7,14 @@ frequency = 293.6648  # частота сигнала в [Гц]
 # т.к. невозможно программно организовать аналоговый сигнал, необходимо обозначить
 fs = 80000 # 80 тыс. временных отсчетов в 1 секунду количество временных отчетов, т.е. частоту дискретизации (об этом чуть позже)
 
-
 timeSamples = np.arange(np.ceil(duration * fs)) / fs
-timeSamples
 
-signal2 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
-signal3 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
-signal4 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
-signal5 = amplitude * np.sin(2 * np.pi * random.randint(100,200) * timeSamples)
+signal2 = amplitude * np.sin(2 * np.pi * 986 * timeSamples)
+signal3 = amplitude * np.sin(2 * np.pi * 1046 * timeSamples)
+signal4 = amplitude * np.sin(2 * np.pi * 1174 * timeSamples)
+signal5 = amplitude * np.sin(2 * np.pi * 1318 * timeSamples)
 signalSumm = signal2 + signal3 + signal4 + signal5
 
-for i  in range(0, 2):
-    sd.play(signal2, fs, None, 1)
-    sd.play(signal3, fs, None, 1)
-    sd.play(signal4, fs, None, 1)
-    sd.play(signal2, fs, None, 1)
-    sd.play(signal3, fs, None, 1)
-
-plt.plot(timeSamples[:2000] * 1000, signalSumm[:2000])
-plt.title("sine tone")
-plt.xlabel("time / milliseconds")
+sum = []
+sum = np.concatenate((signal3,signal2,signal3,signal4,signal5,signal4,signal5,signal3,signal4,signal5), axis=0, out=None, dtype=None, casting="same_kind")
+sd.play(sum, fs, None, 1)
